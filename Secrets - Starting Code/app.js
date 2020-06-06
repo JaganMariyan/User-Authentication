0ffd8193095f549a4fc96cd046fb139c34865628
unknown <ravijagan60@gmailcom>
2020-06-06 14:36:57 +0530

    Level 2 Security

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -3,6 +3,7 @@
  const bodyParser=require("body-parser");
  const ejs=require("ejs");
  const mongoose=require("mongoose");
+ const encrypt=require("mongoose-encryption");
 
  const app=express();
 
@@ -12,10 +13,14 @@
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
 
-const userSchema={
+const userSchema=new mongoose.Schema({
   email:String,
   password:String
-};
+});
+
+const secret="Thisisoursecret";
+
+userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
 
 const User=new mongoose.model("User",userSchema);
  app.get("/",function(req,res){
