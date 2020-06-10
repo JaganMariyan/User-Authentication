be0f244bd80a22852e7512b37cd757f99d70564e
unknown <ravijagan60@gmailcom>
2020-06-11 01:14:11 +0530

    Using Google OAUTH

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const session=require('express-session');
 const passport=require('passport');
 const passportLocalMongoose=require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
+
 
  const app=express();
 
@@ -28,11 +31,13 @@ app.use(passport.session());
 
 const userSchema=new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String
 });
 
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User=new mongoose.model("User",userSchema);
 
@@ -42,8 +47,27 @@ const User=new mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
  app.get("/",function(req,res){
    res.render("home");
  });
@@ -54,6 +78,17 @@ passport.deserializeUser(User.deserializeUser());
   app.get("/register",function(req,res){
     res.render("register");
   });
+  app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+
+
+    app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 
 
   app.get("/secrets",function(req,res){
